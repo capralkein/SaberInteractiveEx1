adeb6cb5686558b5b4b87a6f0ff9a8231877fac3
capralkein <capralkein@yandex.ru>
2022-04-02 12:28:49 +0300

    Add project files.

---
+++ SaberInteractiveEx1/SaberInteractiveEx1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+void RemoveDups(char* pStr);
+
+int main()
+{
+    char Str[] = "AAA BBB AAA";
+
+    printf("%s\n", Str);
+
+    RemoveDups(Str);
+
+    printf("%s\n", Str);
+}
+
+void RemoveDups(char* pStr)
+{
+    if (pStr == nullptr || *pStr == '\0') return;
+
+    int buff = 0;
+    for (int i = 1; pStr[i] != '\0'; i++)
+    {
+        if (pStr[i] != pStr[buff])
+        {
+            pStr[++buff] = pStr[i];
+        }
+    }
+    pStr[buff + 1] = '\0';
+}
